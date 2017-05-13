cc154949832f0a27c9b21fa5675f209928f6a836
Starla <Starla@Applede-MacBook-Pro.local>
2017-05-14 00:13:40 +0800

    choose photo from album and save photo

--- RunRRR/RunRRR/RoundButton.swift
+++ RunRRR/RunRRR/RoundButton.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-class MissionsDetailViewController: UIViewController {
-
+class MissionsDetailViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    var imagePicker: UIImagePickerController!
+    @IBOutlet weak var ShowImage: UIImageView!
     @IBOutlet weak var priorityLabel: PriorityLabel!
     @IBAction func exitMissionDetailButtonTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
@@ -44,6 +45,57 @@ class MissionsDetailViewController: UIViewController {
         
         missionNameLabel.text = mission?.title
         missionContentTextView.text = mission?.content
+    }
+    @IBAction func choosePhoto(_ sender: Any) {
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        let cameraAction = UIAlertAction(title: "USE CAMERA", style: .default){
+            (action) in
+            self.imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .camera)!
+            self.imagePicker.sourceType = .camera
+            self.imagePicker.allowsEditing = true
+            
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }
+        let photoLibraryAction = UIAlertAction(title: "USE PHOTO LIBRARY", style: .default){
+            (action) in
+            self.imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+            self.imagePicker.sourceType = .photoLibrary
+            self.imagePicker.allowsEditing = true
+            
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cameraAction)
+        alertController.addAction(photoLibraryAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let chosenImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        UIImageWriteToSavedPhotosAlbum(chosenImage, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+        ShowImage.image = chosenImage
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func image(_ image: UIImage, didFinishSavingWithError error: NSError?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            // we got back an error!
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
     }
 
 }
